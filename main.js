bcff107a3ac3242b0b777cbf74da1f5b1b4041c6
Hanshal15 <77623196+Hanshal15@users.noreply.github.com>
2021-07-19 19:33:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(550,550);
+    canvas.position(560,150);
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotposes)
+}
+
+function modelLoaded() {
+    console.log("model has been loaded");
+}
+
+function gotposes(results) {
+    if(results.length>0) {
+        console.log(results);
+    }
+}
+
+function draw() {
+    background("#808080");
+}
