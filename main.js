e72d7207630e5007bbfc2b4ca188c06047f4dba1
Hanshal15 <77623196+Hanshal15@users.noreply.github.com>
2021-07-21 19:52:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,12 +15,23 @@ function modelLoaded() {
     console.log("model has been loaded");
 }
 
+rightwristX = 0;
+leftwristX = 0;
+difference=0;
+
 function gotposes(results) {
     if(results.length>0) {
         console.log(results);
+        rightwristX = results[0].pose.rightWrist.x;
+        leftwristX = results[0].pose.leftWrist.x;
+        difference = floor(leftwristX - rightwristX);
     }
 }
 
 function draw() {
     background("#808080");
+    textSize(difference);
+    text("Hanshal",50,400);
+    fill("#0000FF");
+    document.getElementById("font_size").innerHTML = "font size of ur wrist is " + difference + " pixel";
 }
